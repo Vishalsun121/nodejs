d9cfb1bc506cee1dcf76d12916caaf57e3c5b258
Vishalsun121 <Vishalsun121@gmail.com>
2024-09-06 06:55:08 +0000

    Commit new

---
+++ nodejs/test/test.js
@@ -0,0 +1,11 @@
+var request = require('supertest');
+var app = require('../app.js');
+ 
+describe('GET /', function() {
+  it('respond with 404 page not found', function(done) {
+    request(app)
+      .get('/nonexistentpage')
+      .expect(404)
+      .end(function(err, res) {
+        if (err) {
+          // If there's an error, log it and pass it to the done callback
